66b3ade8058b58f343f8d4f05b8873c984a231b5
MPOUDI Andre <andre.mpoudi@gmail.com>
2022-12-14 10:26:29 -0800

    Task 8 completed

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of 'num'
+ * @num: the number to work on
+ *
+ * Return: the last digit of 'num'
+ */
+
+int print_last_digit(int num)
+{
+	int result;
+
+	if (num == 0)
+		result = 0;
+	else
+	{
+		if (num < 0)
+			num = num * -1;
+
+		result = num % 10;
+	}
+
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print all minutes within 24 hours
+ *
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int hour1 = '0';
+	int hour2 = '0';
+	int min1 = '0';
+	int min2 = '0';
+
+	while (hour1 >= '0' && hour1 <= '2')
+	{
+		while (hour2 >= '0' && hour2 <= '3')
+		{
+			while (min1 >= '0' && min1 <= '5')
+			{
+				while (min2 >= '0' && min2 <= '9')
+				{
+					_putchar(min2);
+					_putchar(min1);
+					_putchar(hour2);
+					_putchar(hour1);
+					_putchar('\n');
+					min2++;
+				}
+				min1++;
+				min2 = '0';
+			}
+			hour2++;
+			min1 = '0';
+		}
+		hour1++;
+		hour2 = '0';
+	}
+}
