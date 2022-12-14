9132a9cb0e8bb40eb61e49dd677de2f2f05407cf
MPOUDI Andre <andre.mpoudi@gmail.com>
2022-12-14 01:07:08 -0800

    Task 2 updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
  */
 void print_alphabet_x10(void)
 {
-        char car = 'a';
-        int count = 1;
+	char car = 'a';
+	int count = 1;
 
-        while (count <= 10)
-        {
-                while (car >= 'a' && car <= 'z')
-                {
-                        _putchar(car);
-                        car++;
-                }
-                _putchar('\n');
-                count++;
-        }
+	while (count <= 10)
+	{
+		while (car >= 'a' && car <= 'z')
+		{
+			_putchar(car);
+			car++;
+		}
+		_putchar('\n');
+		count++;
+	}
 }
