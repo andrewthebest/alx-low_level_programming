427918bf57d2583d6df2ad2d7124e4d81f5fbc61
MPOUDI Andre <andre.mpoudi@gmail.com>
2022-12-16 02:20:23 -0800

    Task 9 updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,74 +1,35 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - program that makes "Fizz Buzz test"
+ * main - Fizz-Buzz test
  *
  * Return: 0
  */
 
 int main(void)
 {
+	int min = 1;
+	int max = 100;
 	int i;
-	int n;
-	int num;
-	int mult3;
-	int mult5;
-	int sum = 0;
 
-	while (i <= 100)
+	i = min;
+	while (i < max)
 	{
-		n = i;
-		num = i;
-
-		while (1)
-		{
-			/* Test if number is multiple of 3 */
-			while (n != 0)
-			{
-				sum += (n % 10);
-				n /= 10;
-			}
-
-			if (sum > 9)
-				n = sum;
-			else
-			{
-				if ((sum == 3) || (sum == 6) || (sum == 9))
-					mult3 = 1;
-				else
-					mult3 = 0;
-
-				break;
-			}
-			sum = 0;
-		}
-		/* Test if the number is multiple of 5 */
-		if ((num % 10) == 0 || (num % 10) == 5)
-			mult5 = 1;
-		else
-			mult5 = 0;
-
-		if (mult3 == 1)
+		if ((i % 3) == 0)
 			printf("Fizz");
-		else
-			printf("%d", i);
-
-		if (mult5 == 1)
+		else if ((i % 5) == 0)
 			printf("Buzz");
-		else
-			printf("%d", i);
-
-		if ((mult3 == 1) && (mult5 == 1))
+		else if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
 		else
 			printf("%d", i);
 
-		if (i != 100)
-			_putchar('\n');
+		if (i != max)
+			printf(" ");
 
 		i++;
 	}
+	printf("\n");
 
 	return (0);
 }
