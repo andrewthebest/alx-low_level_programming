a552bcfc809545bdf506bcd0981ff400fd683c21
MPOUDI Andre <andre.mpoudi@gmail.com>
2022-12-16 02:29:34 -0800

    Task 9 updated and completed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,9 +15,9 @@ int main(void)
 	i = min;
 	while (i < max)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 && (i % 5) != 0)
 			printf("Fizz");
-		else if ((i % 5) == 0)
+		else if ((i % 5) == 0 && (i % 3) != 0)
 			printf("Buzz");
 		else if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
