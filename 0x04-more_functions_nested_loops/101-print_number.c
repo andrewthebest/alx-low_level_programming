4de5c57b5ca65d138413441dd2fa5acd7610ec5c
MPOUDI Andre <andre.mpoudi@gmail.com>
2022-12-16 05:44:24 -0800

    Task 101 completed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,5 +9,20 @@
 
 void print_number(int n)
 {
-	
+	int tmp;
+	int car;
+
+	tmp = n;
+
+	if (n < 0)
+	{
+		tmp = tmp * -1;
+		_putchar('-');
+	}
+
+	if ((tmp / 10) > 0) 
+		print_number(num / 10);
+
+	car = num % 10;
+	putchar('0' + car);
 }
