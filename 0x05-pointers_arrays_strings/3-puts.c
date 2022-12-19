0b318cd5f33ad471cb6586d1300f7f26606fb687
MPOUDI Andre <andre.mpoudi@gmail.com>
2022-12-19 12:58:42 -0800

    Task 3 completed

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to standard output
+ * @str: string to print
+ *
+ * Return: Nothing.
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", *str);
+}
