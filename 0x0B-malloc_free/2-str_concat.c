1f53ad0729b92c182addf136356317b6d5933295
MPOUDI Andre <andre.mpoudi@gmail.com>
2023-01-11 15:21:01 -0800

    Update tasks 0-2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 
 	i = 0;
 	if (s1 == NULL)
-		*s1 = "";
+		s1[0] = '\0';
 	if (s2 == NULL)
-		*s2 == "";
+		s2[0] = '\0';
 	if (s1 != NULL)
 		len1 = get_len(s1);
 
