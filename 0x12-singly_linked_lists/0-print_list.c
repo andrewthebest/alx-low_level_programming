66dc585dbdb888ce997752e1d3165ab6114afe4a
MPOUDI Andre <andre.mpoudi@gmail.com>
2023-01-26 15:59:23 -0800

    Task 0 completed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,14 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
-	count = 0;
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		h = h->next;
 		count++;
 	}
