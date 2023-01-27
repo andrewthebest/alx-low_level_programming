18d32c12a67532d1ee5f5e22a18dd73ba3167e8d
MPOUDI Andre <andre.mpoudi@gmail.com>
2023-01-26 17:12:45 -0800

    Advanced tasks completed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 
+void first(void) __attribute__ ((constructor))
+
 /**
  * first - prints a text before the main()
  *
  * Return : Nothing
  */
-void first(void) __attribute__ ((constructor))
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
